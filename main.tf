6eedfde494f51e376580c25e579377f2bf6c4168
Chad A. Carino <artist@chadacarino.com>
2020-11-10 14:39:09 -0700

    call validation submodule and return validation result, as well as labels_map

--- main.tf
+++ main.tf
@@ -5,6 +5,28 @@ locals {
     IVV = "Label value(s) not valid for label key(s)"
     IVR = "Label values are mismatched by relationship"
     GCE = "Google Cloud API error"
+    MMD = "Missing values for BQ validation"
     VLV = "Success"
   }
+}
+
+module "metadata_validation" {
+  source = "github.com/bangerkuwranger/gcp-hm-label-validation-bq-metadata?ref=v0.0.1"
+  metadata_project    = var.metadata_project
+  metadata_dataset    = var.metadata_dataset
+  entity              = var.entity
+  enterprise_platform = var.enterprise_platform
+  bu_flex             = var.bu_flex
+  cost_center         = var,cost_center
+  cost_code           = var.cost_code
+  product             = var.product
+  project             = var.project
+  environment         = var.environment
+}
+
+locals {
+  resource_type_is_valid = contains(local.supported_resource_types, var.resource_type)
+  outcome_code           = local.resource_type_is_valid ? var.metadata_project != "" && var.metadata_dataset != "" ? module.metadata_validation.result.code : "MMD" : "RSP"
+  outcome_string         = local.valid_outcomes[local.outcome_code]
+  outcome_info           = module.metadata_validation.result.info != "" ? ":`\n`${module.metadata_validation.result.info}" : ""
 }
--- output.tf
+++ output.tf
@@ -1,7 +1,18 @@
-output "labels_map" {
-  description = "Key/value set of generated labels"
-}
-
 output "outcome" {
   description = "Result of processing inputs; value is 'Success' if valid label set is generated, otherwise an explanation of why labels cannot be generated"
+  value       = "${local.outcome_string}${local.outcome_info}"
+}
+
+output "labels_map" {
+  description = "Key/value set of generated labels"
+  value       = {
+    entity              = var.entity
+    enterprise_platform = var.enterprise_platform
+    bu_flex             = var.bu_flex
+    cost_center         = var.cost_center
+    cost_code           = var.cost_code
+    product             = var.product
+    project             = var.project
+    environment         = var.environment
+  }
 }
